6fe07f617cd9d71bc4adc02bc965cf07fb5808c6
Annie Rheaume <annie.rheaume@umontreal.ca>
2023-11-10 21:46:59 -0500

    setup

---
+++ script.js
@@ -0,0 +1,41 @@
+/**
+ * TP3
+ * Dernière mise-à-jour : 10-11-2023 (mise-en-place)
+ */
+
+// VUE
+
+// 1 - Linker un script extérieur
+document.querySelector('#main-title').innerHTML = 'Travail Pratique III'
+
+// LES DONNÉES
+
+// Les villes
+
+[
+    {"name": "Montreal", "population": 1762949, "country": "Canada"},
+    {"name": "Toronto", "population": 2794356, "country": "Canada"},
+    {"name": "Vancouver", "population": 662248, "country": "Canada"},
+
+    
+    {"name": "Bogota", "population": 8034649, "country": "Colombie"},
+    {"name": "Medellin", "population": 8034649, "country": "Colombie"},
+
+    
+    {"name": "Alger", "population": 3154792, "country": "Algerie"},
+    {"name": "Oran", "population": 803329, "country": "Algerie"},
+
+    {"name": "Mexico", "population": 9209944, "country": "Mexique"}
+
+]
+
+// Les personnes
+
+[
+    {"name": "Maurice Vachon", "city": "Montreal"},
+    {"name": "Jane Munro", "city": "Vancouver"},
+    {"name": "Eckhart Tolle", "city": "Vancouver"},
+    {"name": "Ingrid Betancourt", "city": "Bogota"},
+    {"name": "DJ Pope", "city": "Medellin"},
+    {"name": "Djaffar Bensetti", "city": "Oran"}   
+]
